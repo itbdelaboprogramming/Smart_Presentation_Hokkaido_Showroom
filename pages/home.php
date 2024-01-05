4cbb9dd581ecf4f007ef777fb0c6a30e3b38f67a
fajarmnrozaki <zakifajar96@gmail.com>
2024-01-05 13:42:09 +0700

    feat: change the portal page design & video background

--- pages/home.php
+++ pages/home.php
@@ -10,16 +10,37 @@
     </head>
     <body>
         <div class="bg">
+
+            <!-- if using the background picture, comment this <video> -->
+             <video autoplay muted loop idate="bgVideo">
+                <source src="./files/light_background.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+             </video>   
+
             <div class="page-home">
-                <p class="title-home">中山鉄工所北海道ショールーム</p>
+                <!-- <p class="title-home">中山鉄工所北海道ショールーム</p> -->
     
                 <div class="button-container">
-                    <a class="button-404" onclick="location.reload()">Reload Page</a>
-                    <a class="button-404" href="crushing-plant">Diorama1</a>
-                    <a class="button-404" href="recycling-plant">Diorama2</a>
+                    <a class="button-404" href="crushing-plant">
+                    <img src="./assets/Crushing Plant.png" alt="Crushing Plant">
+                        <span>破砕プラントの3Dモデル</span>
+                    </a>
+                    
+                    <a class="button-404" href="recycling-plant">
+                    <img src="./assets/Recycling Plant.png" alt="Recycling Plant">
+                        <span>リサイクル工場の3Dモデル</span>
+                    </a>
                 </div>
-            </div>
-        </div>
 
+                <div class="button-container-2">
+                    <a class="button-movie" onclick="location.reload()">
+                        <img src="./assets/movie.png" alt="Company Information">
+                            <!-- <span>Company Information</span> -->
+                    </a>                    
+                </div>
+
+            </div>
+
+        </div>
     </body>
 </html>
