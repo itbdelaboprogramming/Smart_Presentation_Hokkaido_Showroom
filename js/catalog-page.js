55cd78e471dcbbc20666df4f4e571bda1a279aa6
adeliaaaa <82143550+adeliaaaa@users.noreply.github.com>
2024-05-20 22:43:59 +0700

    fix pop up pdf bug

--- js/catalog-page.js
+++ js/catalog-page.js
@@ -2,14 +2,18 @@ const pdfCards = document.querySelectorAll(".pdf-card");
 const pdf_pop_up = document.querySelector(".container-full-screen-pdf");
 const pdf_file = document.getElementById("pdf-file");
 // const full_screen_pdf = document.querySelector(".full-screen-pdf.active");
+let currentPdfCard = null;
 
 pdfCards.forEach(function (pdfCard) {
 	pdfCard.addEventListener("click", function () {
 		pdf_pop_up.classList.add("active");
-		pdf_file.setAttribute(
-			"src",
-			pdfCard.getAttribute("data-pdf") + "#scrollbar=0&toolbar=0&view=FitH"
-		);
+		if (currentPdfCard !== pdfCard.getAttribute("data-pdf")) {
+			currentPdfCard = pdfCard.getAttribute("data-pdf");
+			pdf_file.setAttribute(
+				"src",
+				currentPdfCard + "#scrollbar=0&toolbar=0&view=FitH"
+			);
+		}
 	});
 });
 
