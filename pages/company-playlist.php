1b56646b022aab35d6834eb6dda735ef53183116
fajarmnrozaki <zakifajar96@gmail.com>
2024-10-29 17:35:46 +0700

    feat: change the text and file path url

--- pages/company-playlist.php
+++ pages/company-playlist.php
@@ -13,25 +13,35 @@
     </div>
     <div class="company-page">
         <div class="container-top-left">
-            <button class="menu-container-back-button" onClick="location.href='home'">
+            <button class="menu-container-back-button" onClick="location.href='company-video'">
                 <img src="./assets/Back-Button.svg"> Back
             </button>
             <div class="page-name-container">
-                <div class="page-name-text">Lorem Ipsum</div>
+                <div class="page-name-text">YouTubeの動画一覧</div>
             </div>
         </div>
         <div class="playlist-content-container">
             <?php
                 $playlists = array(
                     "playlist-card-1" => [
-                        "image" => "2.png",
+                        "image" => "1.jpeg",
                         "title" => "インフォメーション",
                         "iframe" => '<iframe class="playlist-iframe" src="https://www.youtube.com/embed/videoseries?si=o6u2D9KPEwrqaV8V&amp;list=PLxI9XPCxiURH7h_E6QMvjYRZSEKwX2CFJ&autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
                     ],
                     "playlist-card-2" => [
-                        "image" => "1.png",
+                        "image" => "2.jpeg",
                         "title" => "Dendoman",
                         "iframe" => '<iframe class="playlist-iframe" src="https://www.youtube.com/embed/videoseries?si=PWMCUEK1A77sk__S&amp;list=PLxI9XPCxiUREQLEkfxpPXVD923L_SHh_N&autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+                    ],
+                    "playlist-card-3" => [
+                        "image" => "3.jpeg",
+                        "title" => "プラント / N-Link",
+                        "iframe" => '<iframe class="playlist-iframe" src="https://www.youtube.com/embed/videoseries?si=tHxwm2KvtHVcOWzA&amp;list=PLxI9XPCxiURFHHGkg3O33vRQx8OjoYRBi&autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+                    ],
+                    "playlist-card-4" => [
+                        "image" => "4.jpeg",
+                        "title" => "クラッシャ",
+                        "iframe" => '<iframe class="playlist-iframe" src="https://www.youtube.com/embed/videoseries?si=OrXaQo7B1K-j6MPF&amp;list=PLxI9XPCxiURGiBNhlHyFxdHKJ4HOFu6Rw&autoplay=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
                     ],
                 );
                 foreach ($playlists as $id => $data) {
@@ -40,16 +50,16 @@
                     $iframe = $data["iframe"];
                     echo <<<EOD
                     <div class="playlist-card" id="$id" data-iframe='$iframe'>
-                        <img class="playlist-thumbnail" src="/files/img/playlists/$image"/>
+                        <img class="playlist-thumbnail" src="./files/img/playlists/$image"/>
                         <div class="playlist-title">$title</div>
                     </div>
                     EOD;
                 }
             ?>
         </div>
-        <div class="bottom-center">
+        <!-- <div class="bottom-center">
             <div class="text-page">Lorem ipsum odor amet, consectetuer adipiscing elit.</div>
-        </div>
+        </div> -->
     </div>
 </body>
 </html>
